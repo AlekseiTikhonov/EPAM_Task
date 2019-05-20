62d150d4a4fd9d5d80eff283dcb53a1f27243712
AlekseiTikhonov <a.a.tikhonov86@gmail.com>
2019-05-20 20:09:47 +0500

    Water and Air classes were added

---
+++ TransportCompany/TransportCompany/AirTransport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportCompany
+{
+    class AirTransport : Vehicle
+    {
+        private string typeAir; // категория техники (самолёты, вертолёты)
+
+        public void SetTypeAir(string typeOfAirVehicle)
+        {
+            typeAir = typeOfAirVehicle;
+        }
+        public string GetTypeAirVehicle()
+        {
+            return typeAir;
+        }
+
+        public AirTransport() : base()
+        {
+            typeAir = "Нет типа";
+        }
+
+        public AirTransport(string marka, int speed, int carrying, int numOfStuff, int numOfPassengers, string typeOfVehicle, string typeOfAirVehicle)
+            : base(marka, speed, carrying, numOfStuff, numOfPassengers, typeOfVehicle)
+        {
+            typeAir = typeOfAirVehicle;
+        }
+    }
+}
---
+++ TransportCompany/TransportCompany/WaterTransport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportCompany
+{
+    class WaterTransport : Vehicle
+    {
+        private string typeWater; // категория техники (лайнеры, корабли)
+
+        public void SetTypeWater(string typeOfWaterVehicle)
+        {
+            typeWater = typeOfWaterVehicle;
+        }
+        public string GetTypeWaterVehicle()
+        {
+            return typeWater;
+        }
+
+        public WaterTransport() : base()
+        {
+            typeWater = "Нет типа";
+        }
+
+        public WaterTransport(string marka, int speed, int carrying, int numOfStuff, int numOfPassengers, string typeOfVehicle, string typeOfWaterVehicle)
+            : base(marka, speed, carrying, numOfStuff, numOfPassengers, typeOfVehicle)
+        {
+            typeWater = typeOfWaterVehicle;
+        }
+    }
+}
